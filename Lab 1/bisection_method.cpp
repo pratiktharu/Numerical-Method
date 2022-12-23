f3052e903ae4dd741f5f826fc0a4a92e4460a321
Pratik Tharu <077BCT060.pratik@pcampus.edu.np>
2022-12-23 07:28:13 +0545

    Bisection Method

---
+++ Lab 1/bisection_method.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+double checkRoot(double x){
+    return x*sin(x)+cos(x);
+}
+
+int main()
+{
+
+    double a,b;
+    int count=0;
+    double c=0;
+    cout<<"Enter Interval"<<endl;
+    cin>>a>>b;
+    if(checkRoot(a)*checkRoot(b)>0){
+
+        std::cout<<"Root doesnot lie in this interal";
+
+    }
+    else{
+
+        while(fabs(checkRoot(c)>0.0005)){
+
+            count++;
+            c=(a+b)/2;
+            if(checkRoot(a)*checkRoot(c)<0){
+                b=c;
+            }
+            else{
+                a=c;
+
+            }
+            if(count==50)
+            {
+                cout<<"The function is discountinous";
+            }
+        }
+    }
+
+    cout<<"The root is"<<c;
+    return 0;
+
+
+}
